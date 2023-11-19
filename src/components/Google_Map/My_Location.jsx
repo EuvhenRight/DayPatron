1a21910c1d7ed9d256f9dda86be6fe8211a262a0
EuvhenRight <ugnivenko.ea@gmail.com>
2023-11-20 00:02:08 +0100

    change style dark

--- src/components/Google_Map/My_Location.jsx
+++ src/components/Google_Map/My_Location.jsx
@@ -3,7 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { useMemo } from 'react';
 import styles from './style.json';
 
-const MyLocation = () => {
+export default function MyLocation() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyAbYfcyZaQ0DRQbYAwgYVEjQMKmXlo-f7E',
   });
@@ -17,9 +17,10 @@ const MyLocation = () => {
   }
 
   return <Map />;
-};
+}
 
 const Map = () => {
+  const { colorMode } = useColorMode();
   const center = useMemo(() => ({ lat: 50.42015, lng: 30.5201551 }), []);
   const mapContainerStyle = {
     width: '100%',
@@ -27,16 +28,18 @@ const Map = () => {
     borderRadius: '2em',
   };
 
+  const mapOptions = {
+    styles: colorMode === 'dark' ? styles : undefined,
+  };
+
   return (
     <GoogleMap
       zoom={14}
       center={center}
       mapContainerStyle={mapContainerStyle}
-      options={{ styles: styles }}
+      options={mapOptions}
     >
       <Marker position={center} />
     </GoogleMap>
   );
 };
-
-export default MyLocation;
