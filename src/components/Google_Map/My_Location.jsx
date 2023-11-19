5170e9a84fa52c2eb586d2e47da8926311865045
EuvhenRight <ugnivenko.ea@gmail.com>
2023-11-19 23:16:34 +0100

    change style map

--- src/components/Google_Map/My_Location.jsx
+++ src/components/Google_Map/My_Location.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { useMemo } from 'react';
+import styles from './style.json';
 
 const MyLocation = () => {
   const { isLoaded, loadError } = useLoadScript({
@@ -24,6 +25,12 @@ const Map = () => {
     width: '100%',
     height: '400px',
     borderRadius: '2em',
+    disableDefaultUI: true, // disable default map UI
+    draggable: true, // make map draggable
+    keyboardShortcuts: false, // disable keyboard shortcuts
+    scaleControl: true, // allow scale controle
+    scrollwheel: true, // allow scroll wheel
+    styles: styles, // change default map styles
   };
 
   return (
